800aeacc9fa042e52b368b7f296a966001df9bdb
RobertoAbad995 <roberto.ramirez@enhanceit.us>
2022-06-14 16:54:20 -0400

    Plus: add a pager

--- RickAndMortyAPI/ViewController.swift
+++ RickAndMortyAPI/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class ViewController: UIViewController {
     
     var items : [Character] = []
-
+    var index : Int = 1
+    var param : String = ""
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -19,11 +21,40 @@ class ViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
-        
-        let request = CharactersAPI().getCharacters(params: "") as! CharacterRequest
-        items = request.results
+        loadData()
     }
 
+    private func loadData(){
+        let request = CharactersAPI().getCharacters(params: param, pageIndex: index) as! CharacterRequest
+        items = request.results
+        
+        tableView.reloadData()
+    }
 
+    @IBAction func btnBoth(_ sender: Any) {
+        param = ""
+        index = 1
+        loadData()
+    }
+    @IBAction func btnAlive(_ sender: Any) {
+        index = 1
+        param = "alive"
+        loadData()
+    }
+    @IBAction func btnDead(_ sender: Any) {
+        param = "dead"
+        index = 1
+        loadData()
+    }
+    @IBAction func btnPrevious(_ sender: Any) {
+        index -= 1
+        index = index < 0 ? 0:index
+        loadData()
+        
+    }
+    @IBAction func btnNext(_ sender: Any) {
+        index += 1
+        loadData()
+    }
 }
 
--- RickAndMortyAPI/networkingProviders/CharactersR&M_API.swift
+++ RickAndMortyAPI/networkingProviders/CharactersR&M_API.swift
@@ -9,31 +9,38 @@ import Foundation
 
 struct CharactersAPI{
     
-    let baseURL:String = "https://rickandmortyapi.com/api/"
+    let baseURL:String = "https://rickandmortyapi.com/api/character/?"
         
-        func getCharacters(params:String) -> CharacterRequest{
+    func getCharacters(params:String, pageIndex: Int = 1) -> CharacterRequest{
+        
+        var urlEndpoint = baseURL + "page=\(pageIndex)"
+        
+        if(!params.isEmpty){
+            urlEndpoint += "&status=" + params
+        }
             
-            let urlEndpoint = baseURL + "character"
             
-            var returnData = CharacterRequest()
-            //create the url by urlString
-            guard let url = URL(string: urlEndpoint) else {
-                print("Invalid url...")
-                return returnData
-            }
-            //create a semaophore to wait until get the response
-            let semaphore = DispatchSemaphore(value: 0)
-            URLSession.shared.dataTask(with: url) { data, response, error in
-                let dataRecived = try! JSONDecoder().decode(CharacterRequest.self, from: data!)
-                DispatchQueue.global().sync {
-                    returnData = dataRecived
-                    //response is correct, semaphore set signal
-                    semaphore.signal()
-                }
-            }.resume()
-            //semaphore wait until response is correct
-            _ = semaphore.wait(wallTimeout: .distantFuture)
+        var returnData = CharacterRequest()
+        //create the url by urlString
+        guard let url = URL(string: urlEndpoint) else {
+            print("Invalid url...")
             return returnData
         }
+        //create a semaophore to wait until get the response
+        let semaphore = DispatchSemaphore(value: 0)
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            let dataRecived = try! JSONDecoder().decode(CharacterRequest.self, from: data!)
+            DispatchQueue.global().sync {
+                returnData = dataRecived
+                //response is correct, semaphore set signal
+                semaphore.signal()
+            }
+        }.resume()
+        //semaphore wait until response is correct
+        _ = semaphore.wait(wallTimeout: .distantFuture)
+        return returnData
+    }
+    
+    
     
 }
